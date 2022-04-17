433e11310ba3503085a935c16354cef8b1388e24
zsun72 <103862547+zsun72@users.noreply.github.com>
2022-04-16 23:44:13 -0400

    Add files via upload

---
+++ board.py
@@ -0,0 +1,13 @@
+board = [['-','-','-'],
+         ['-','-','-'],
+         ['-','-','-'],]
+
+def print_board():
+    for row in board:
+        print(row)
+
+def main():
+    print('Testing')
+    print_board()
+
+main()
